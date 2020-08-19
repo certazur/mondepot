b4cba0ccabe1e733e841da9f049423cbebd6be51
certazur <69810159+certazur@users.noreply.github.com>
2020-08-19 17:04:58 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,5 +2,5 @@
 
 sudo apt update && sudo apt upgrade -y 
 sudo apt install -y nginx 
-#sudo echo "Hello from host $(hostname)" > /var/www/html/index.html 
+#sudo echo "Hello from host $(hostname), installation d'extension customscript réussie" > /var/www/html/index.html 
 
